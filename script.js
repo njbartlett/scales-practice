285b7fdf4dd1930fadda83b9d83d0d586b769fe8
Neil Bartlett <njbartlett@gmail.com>
2021-06-25 09:56:03 +0100

    removed voice selection code

--- script.js
+++ script.js
@@ -1,23 +1,6 @@
-// get all voices that browser offers
-var available_voices = window.speechSynthesis.getVoices();
-// this will hold an english voice
-var english_voice = '';
-
-// find voice by language locale "en-US"
-// if not then select the first voice
-for(var i=0; i<available_voices.length; i++) {
-    if(available_voices[i].lang === 'en-GBgi') {
-        english_voice = available_voices[i];
-        break;
-    }
-}
-if(english_voice === '') {
-    english_voice = available_voices[0];
-}
 var utter = new SpeechSynthesisUtterance();
 utter.rate = 1;
 utter.pitch = 1.0;
-// utter.voice = english_voice;
 
 function speak(selectedCombo) {
     var text = selectedCombo.section + ", " + selectedCombo.playing + ", " + selectedCombo.detail
